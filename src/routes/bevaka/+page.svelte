497f7800f334b45f4affb5c69c193b7f7598b7ae
Fredrik Hammar <fhammar@volvocars.com>
2024-05-05 12:26:05 +0200

    add form and offline course selection

--- src/routes/bevaka/+page.svelte
+++ src/routes/bevaka/+page.svelte
@@ -1,4 +1,68 @@
+<script>
+	let availableCourses = ['Ängen', 'Queens'];
+	let courses = [];
+
+	$: console.log(courses);
+
+</script>
+
+
 
 <h1>
 	Bevaka
 </h1>
+
+<form method="POST">
+
+	<label>
+		Datum
+		<input name="date" type="date">
+	</label>
+	<label>
+		Från klockan
+		<input name="fromTime" type="time">
+	</label>
+	<label>
+		Till klockan
+		<input name="toTime" type="time">
+	</label>
+	<label>
+		Antal spelare
+		<input name="players" type="number">
+	</label>
+	<label>
+		Bana/Banor
+
+		{#each availableCourses as courseOption}
+			<label>
+				<input type="checkbox" bind:group={courses} value={courseOption} />
+				{courseOption}
+			</label>
+		{/each}
+
+	</label>
+	<label>
+		Email
+		<input name="email" type="email">
+	</label>
+	<button>Starta bevakning</button>
+</form>
+
+<style>
+	h1 {
+		text-align: center;
+	}
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	label {
+		display: flex;
+		flex-direction: column;
+		margin: 10px;
+	}
+	input {
+		margin-top: 5px;
+	}
+</style>
